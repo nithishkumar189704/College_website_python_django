08e885d948ed6be672fff018f819edcb3d4cdb20
nithishkumar189704 <nithishkumar189704@gmail.com>
2024-12-19 13:24:01 +0530

    collegemanagement

--- collegewebsite/urls.py
+++ collegewebsite/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.url')),
+    path('about/', views.about, name='about')
 ]
